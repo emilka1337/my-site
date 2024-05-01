336dbec096d6595a34621d8efb1f5e106794c261
Emil <maxmudov2000@gmail.com>
2024-05-01 23:18:32 +0400

    my-projects section added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 document.querySelector('.navbar-toggler').addEventListener("click", () => {
     document.querySelector('.navbar>ul').classList.toggle("show");
 });
+
+document.querySelector('#scrollToTop').addEventListener("click", () => window.scroll({ top: 0, behavior: "smooth" }));
